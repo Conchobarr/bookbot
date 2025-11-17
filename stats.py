f5321ed05b3721a8935b93cc3cfffac4ae9ee1b0
“Conchobarr” <connortpear@gmail.com>
2025-11-17 15:59:42 -0800

    added character count function to stats.py

--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
 def word_count(text):
     num_words = len(text.split())
     return num_words
+
+def get_character_count(text):
+    num_characters = {
+    }
+    lowered_text = text.lower()
+    for char in lowered_text:
+        num_characters[char] += 1
+    return num_characters
