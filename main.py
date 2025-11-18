d56f030b74b42cdaaeeec88bb3ffa684da2ff6ef
“Conchobarr” <connortpear@gmail.com>
2025-11-17 17:20:57 -0800

    added sort_character_count function to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, get_character_count
+from stats import word_count, get_character_count, sort_character_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,8 +8,9 @@ def get_book_text(path_to_file):
 def main():
     file_path = "books/frankenstein.txt"
     book = get_book_text(file_path)
+    num_characters = get_character_count(book)
     print(f"Found {word_count(book)} total words")
-    print(f"{get_character_count(book)}")
-
+    #print(num_characters)
+    print(sort_character_count(num_characters))
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,14 @@ def get_character_count(text):
             num_characters[f'{char}'] += 1
         
     return num_characters
+
+def sort_character_count(character_count):
+    characters = []
+    for key in character_count:
+        dict_entry = {"char": key, "count": character_count[key]}
+        characters.append(dict_entry)
+    #print(unsorted_characters)
+    # list.sort() sorts in-place and returns None, so call it and then
+    # return the list itself. Sort by 'count' (highest first).
+    characters.sort(key=lambda x: x['count'], reverse=True)
+    return characters
